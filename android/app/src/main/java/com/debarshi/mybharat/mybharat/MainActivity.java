31975a297c2d27c2a5379dbdac86209120b3a797
Debarshi Chakma <noxivu@gmail.com>
2023-09-07 07:58:00 +0530

    1.0.1 First Push with Random Splash Screen

---
+++ android/app/src/main/java/com/debarshi/mybharat/mybharat/MainActivity.java
@@ -0,0 +1,6 @@
+package com.debarshi.mybharat.mybharat;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/main.dart
@@ -0,0 +1,62 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'My Bharat App',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: const MyHomePage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 2),
+            ()=>Navigator.pushReplacement(context,
+            MaterialPageRoute(builder:
+                (context) =>
+            const SecondScreen()
+            )
+        )
+    );
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        color: Colors.white,
+        child:FlutterLogo(size:MediaQuery.of(context).size.height)
+    );
+  }
+}
+class SecondScreen extends StatelessWidget {
+  const SecondScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title:const Text("MyBharat")),
+      body: const Center(
+          child:Text("Welcome Dost!",textScaleFactor: 2,)
+      ),
+    );
+  }
+}
